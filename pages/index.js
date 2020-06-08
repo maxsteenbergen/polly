56487bdc1bcfa85a7ec36a831f7ea2f999e374a1
⛏ <maxsteenbergen@gmail.com>
2020-06-08 22:23:15 +0200

    Initial commit
    V0.1 supports flashcards for letters, duo-sounds, those two combined and words

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,80 @@
+import {useEffect,useState,useRef} from 'react'
 import Head from 'next/head'
+import styled from "styled-components";
+import baffle from "baffle"
+import {Words, Duosounds, Letters} from '../resources/dictionary.js'
+
+const Container = styled('main')`
+  width: 100vw;
+  height: 100vh;
+  background: hsl(100,80%, 75%);
+  & menu {
+    position: absolute;
+    left: 0;
+    top: 0;
+    display: flex;
+    height: 48px;
+    width: 100%;
+    justify-content: center;
+    & img {
+     width: 100px;
+     opacity: 0.3;
+     &.active {
+      opacity: 1;
+     }
+    }
+  }
+    & div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+    width: 100%;
+    height: 100%;
+    }
+`
+const ContentNode = styled('h1')`
+  text-align: center;
+  font-size: 15vmax;
+  color: hsla(0,0%,0%,0.8);
+  user-select: none;
+  width: 100%;
+  cursor: pointer;
+  font-weight: 300;
+`
 
 export default function Home() {
+  const ContentRef = useRef(null)
+  const [Content, setContent] = useState('')
+  const [ActiveDictionary, setActiveDictionary] = useState(Words)
+
+  useEffect(()=>{
+    baffle(ContentRef.current)
+      .set({characters: '░▒▓!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_'})
+      .reveal(5000);
+  },[Content])
+
+  const changeCharacter = () => {
+    const randomIndex = Math.floor(Math.random() * (ActiveDictionary.length - 1));
+    setContent(ActiveDictionary[randomIndex])
+  }
+  useEffect(changeCharacter,[ActiveDictionary])
+
   return (
-    <div className="container">
+    <Container>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <menu>
+        <img className={ActiveDictionary === Letters ? 'active' : null} onClick={()=>{setActiveDictionary(Letters)}} alt="Woorden" src={'/img/letter.svg'} />
+        <img className={ActiveDictionary === Duosounds  ? 'active' : null} onClick={()=>{setActiveDictionary(Duosounds)}} alt="Letters" src={'/img/duo.svg'} />
+        <img className={ActiveDictionary !== Letters && ActiveDictionary !== Duosounds && ActiveDictionary !== Words ? 'active' : null} onClick={()=>{setActiveDictionary([...Letters, ...Duosounds])}} alt="Dubbelklanken" src={'/img/letter+duo.svg'} />
+        <img className={ActiveDictionary === Words ? 'active' : null} onClick={()=>{setActiveDictionary(Words)}} alt="Letters en dubbelklanken" src={'/img/word.svg'} />
+      </menu>
+      <div onClick={changeCharacter} >
+        <ContentNode ref={ContentRef}>{Content}</ContentNode>
+      </div>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+    </Container>
   )
 }
