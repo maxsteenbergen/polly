adc6096c6a83dd708009778515223b1e9c524eea
⛏ <maxsteenbergen@gmail.com>
2020-06-09 22:06:26 +0200

    Add support for numbers

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles.css'
+import 'rc-slider/assets/index.css';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import {useEffect,useState,useRef} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import Head from 'next/head'
 import styled from "styled-components";
 import baffle from "baffle"
-import {Words, Duosounds, Letters} from '../resources/dictionary.js'
+import {Duosounds, Letters, LettersAndSounds, Words} from '../resources/dictionary.js'
+import Slider from 'rc-slider'
+
 
 const Container = styled('main')`
   width: 100vw;
@@ -24,13 +26,14 @@ const Container = styled('main')`
      }
     }
   }
-    & div {
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  & > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
     width: 100%;
     height: 100%;
-    }
+    cursor: pointer;
+  }
 `
 const ContentNode = styled('h1')`
   text-align: center;
@@ -38,27 +41,44 @@ const ContentNode = styled('h1')`
   color: hsla(0,0%,0%,0.8);
   user-select: none;
   width: 100%;
-  cursor: pointer;
   font-weight: 300;
 `
 
 export default function Home() {
   const ContentRef = useRef(null)
   const [Content, setContent] = useState('')
+  const [MaxNumber, setMaxNumber] = useState(20)
   const [ActiveDictionary, setActiveDictionary] = useState(Words)
 
   useEffect(()=>{
     baffle(ContentRef.current)
       .set({characters: '░▒▓!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_'})
-      .reveal(5000);
+      .start()
+      .reveal(100,200);
   },[Content])
 
   const changeCharacter = () => {
-    const randomIndex = Math.floor(Math.random() * (ActiveDictionary.length - 1));
-    setContent(ActiveDictionary[randomIndex])
-  }
-  useEffect(changeCharacter,[ActiveDictionary])
+    if(typeof ActiveDictionary === 'string' && ActiveDictionary === 'Numbers'){
+      const min = 0;
+      const randomNumber = Math.floor(Math.random() * (MaxNumber - min ) + min)
+      setContent(randomNumber)
+    }
+    else {
+      const randomIndex = Math.floor(Math.random() * (ActiveDictionary.length - 1));
+      setContent(ActiveDictionary[randomIndex])
+    }
 
+  }
+  useEffect(changeCharacter,[ActiveDictionary, MaxNumber])
+
+  const marks = {
+    0: {style: {fontSize: 16,}, label:0},
+    10: {style: {fontSize: 16,}, label:10},
+    20: {style: {fontSize: 16,}, label:20},
+    30: {style: {fontSize: 16,}, label:30},
+    50: {style: {fontSize: 16,}, label:50},
+    100: {style: {fontSize: 16,}, label:100},
+  };
   return (
     <Container>
       <Head>
@@ -68,9 +88,15 @@ export default function Home() {
       <menu>
         <img className={ActiveDictionary === Letters ? 'active' : null} onClick={()=>{setActiveDictionary(Letters)}} alt="Woorden" src={'/img/letter.svg'} />
         <img className={ActiveDictionary === Duosounds  ? 'active' : null} onClick={()=>{setActiveDictionary(Duosounds)}} alt="Letters" src={'/img/duo.svg'} />
-        <img className={ActiveDictionary !== Letters && ActiveDictionary !== Duosounds && ActiveDictionary !== Words ? 'active' : null} onClick={()=>{setActiveDictionary([...Letters, ...Duosounds])}} alt="Dubbelklanken" src={'/img/letter+duo.svg'} />
+        <img className={ActiveDictionary === LettersAndSounds ? 'active' : null} onClick={()=>{setActiveDictionary(LettersAndSounds)}} alt="Dubbelklanken" src={'/img/letter+duo.svg'} />
         <img className={ActiveDictionary === Words ? 'active' : null} onClick={()=>{setActiveDictionary(Words)}} alt="Letters en dubbelklanken" src={'/img/word.svg'} />
+        <img className={ActiveDictionary === 'Numbers' ? 'active' : null} onClick={()=>{setActiveDictionary('Numbers')}} alt="Letters en dubbelklanken" src={'/img/numbers.svg'} />
       </menu>
+      {ActiveDictionary === 'Numbers' &&
+      <div style={{width: 400, marginTop: 50, height: 24, position: 'absolute', left: '50%', transform: 'translate(-50%)', userSelect: 'none'}}>
+        <Slider dots min={-10} marks={marks} step={10} onChange={v=>setMaxNumber(v)} defaultValue={MaxNumber}/>
+      </div>
+      }
       <div onClick={changeCharacter} >
         <ContentNode ref={ContentRef}>{Content}</ContentNode>
       </div>
